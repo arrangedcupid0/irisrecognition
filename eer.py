487f4261fe002172e117010b878face64e14ce87
arrangedcupid0 <mstoll3@uwyo.edu>
2026-01-21 18:16:49 -0700

    eer

---
+++ eer.py
@@ -0,0 +1,10 @@
+import cv2
+import iris
+import os
+import numpy as np
+
+#for num in range(1,22):
+	impArr = np.loadtxt(f"/home/arrangedcupid0/irisrecognition/users/user{num}/impArr.txt", delimiter=",")
+	
+	genArr = np.loadtxt(f"/home/arrangedcupid0/irisrecognition/users/user{num}/genArr.txt", delimiter=",")
+	
